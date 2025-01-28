6ac4287c20e87fbf1f6514e935a19aee82a74b28
Daniel Chote <daniel@chote.com>
2025-01-27 23:27:30 -0500

    style updates and tweaks

--- src/components/Cocktail.vue
+++ src/components/Cocktail.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card
     class="ma-3 mx-auto d-flex flex-column"
-    width="340"
+    width="360"
   >
     <v-img
       v-if="cocktail.images && cocktail.images[0]"
@@ -10,16 +10,36 @@
       :src="cocktail.images[0].url"
       cover
     >
-      <v-card-title style="text-shadow: 1px 1px 2px rgba(0,0,0, 0.8)">{{ cocktail.name }}</v-card-title>
+      <v-card-title style="text-shadow: 1px 1px 2px rgba(0,0,0, 0.9)">{{ cocktail.name }}</v-card-title>
     </v-img>
     <v-card-title v-else>{{ cocktail.name }}</v-card-title>
     
-    <v-card-text v-if="cocktail.description" v-html="cocktail.description"></v-card-text>
+    <v-row 
+      v-if="cocktail.ingredients.length > 0" 
+      class="mx-3 mt-3"
+    >
+      <v-chip
+        v-for="ingredient in cocktail.ingredients" :key="ingredient.sort"
+        class="ma-1"
+        size="x-small"
+        variant="tonal"
+        label
+      >
+        {{ ingredient.ingredient.name }}
+      </v-chip>
+    </v-row>
+    
+    <v-card-text 
+      v-if="cocktail.description" 
+      v-html="cocktail.description"
+      style="display:inline-block; height: 100px; overflow: hidden; text-overflow: ellipsis;"
+    >
+    </v-card-text>
     <v-card-text v-else class="text-disabled">No Description.</v-card-text>
     
     <v-card-actions class="align-bottom">
       <v-btn
-        class="mt-auto mx-auto"
+        class="mx-auto"
         size="small"
         text="View Recipe"
         variant="outlined"
